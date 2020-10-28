502c2d7cf84bf65a549a3993242b2a07b4e35693
rostyslavnikitin <rosstindev@gmail.com>
2020-10-28 18:13:42 +0200

    add base functions to retrieve data

--- src/currencies.js
+++ src/currencies.js
@@ -1,2 +1,16 @@
-const currencies = ["usd", "uah", "rub"];
-export default currencies;
+import axios from "axios";
+
+class CurrencyConverter {
+    async currenciesList() {
+        const res = await axios.get("/currencies");
+        return res.data.split('\n');
+    }
+
+    async getLatestRate(fromcode, tocode) {
+        const res = await axios.get(`/getlatest/${fromcode}/${tocode}`);
+        console.log(res.data);
+        return res.data
+    }
+}
+
+export default CurrencyConverter;
