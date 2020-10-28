a3ee2ce1cd526a8727b8bd7b689331cbcfdfc7a9
rostyslavnikitin <rosstindev@gmail.com>
2020-10-28 16:14:47 +0200

    add converter component

---
+++ src/converter/converter.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import currencies from "../currencies";
+export default class Converter extends Component {
+    render() {
+        const currenciesOptions = [];
+
+        for(let c of currencies) {
+            currenciesOptions.push(<option value={c}>{c}</option>)
+        }
+
+        return (
+            <div>
+                <form>
+                    <input name="c_from"/>
+                    <select name="from">
+                        {currenciesOptions}
+                    </select>
+                    <input name="c_to"/>
+                    <select name="to">
+                        {currenciesOptions}
+                    </select>
+                </form>
+            </div>
+        );
+    }
+}
