e5e9970029d02a55edf1bf9932a5c1de65200127
rostyslavnikitin <rosstindev@gmail.com>
2020-10-28 19:24:42 +0200

    complete basic converter functionality

--- src/converter/converter.js
+++ src/converter/converter.js
@@ -4,29 +4,59 @@ import CurrencyConverter from "../currencies";
 export default class Converter extends Component {
     constructor(props) {
         super(props);
-        this.state = { data: [] };
+        this.cv = new CurrencyConverter();
+
+        this.state = { data: [], valueFrom: 100, rate: 0, currSource: "USD", currDest: "UAH" };
+
+        this.s_ref = React.createRef();
+        this.d_ref = React.createRef();
     }
 
-    componentDidMount () {
-        const cv = new CurrencyConverter();
-        cv.currenciesList()
+    updateResult = async () => {
+        const data = await this.cv.getLatestRate(this.state.currSource, this.state.currDest);
+        this.setState({ rate: data });
+    };
+
+    componentDidMount = async () => {
+        this.cv.currenciesList()
             .then(data =>  {
                 this.setState({data});
             });
-    }
+        await this.updateResult();
+    };
+
+    // handle original amount value changing
+    fromValueChange = async (event) => {
+        await this.setState({ valueFrom: event.target.value });
+    };
+
+    // handle source currency change
+    fromCurrChange = async (event) => {
+        await this.setState({ currSource: event.target.value});
+        await this.updateResult();
+    };
+
+    // handle dest currency change
+    toCurrChange = async (event) => {
+        await this.setState({ currDest: event.target.value });
+        await this.updateResult();
+    };
 
     render() {
-
-
         return (
             <div>
+                <div>
+                    from: <b>{this.state.currSource}</b>
+                    to: <b>{this.state.currDest}</b>
+                    rate: <b>{this.state.rate}</b>
+                </div>
                 <form>
-                    <input name="c_from"/>
-                    <select name="from">
+                    <input name="c_from" value={this.state.valueFrom} onChange={this.fromValueChange} type="number" />
+                    <select value={this.state.currSource} name="from" onChange={this.fromCurrChange}>
                         {this.state.data.map((code) => (<option value={code} key={code}>{code}</option>))}
                     </select>
-                    <input name="c_to" readOnly="readOnly"/>
-                    <select name="to">
+                    <input name="c_to" readOnly="readOnly" value={this.state.valueFrom * this.state.rate}/>
+                    <select value={this.state.currDest} name="to" onChange={this.toCurrChange}>
                         {this.state.data.map((code) => (<option value={code} key={code}>{code}</option>))}
                     </select>
                 </form>
