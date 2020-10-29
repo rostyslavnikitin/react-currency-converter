552c75da6a4f3de3bb171e6103cb86b33cbc0f38
rostyslavnikitin <rosstindev@gmail.com>
2020-10-29 11:50:38 +0200

    update currency table to use currency array and display exchange rate

--- src/currencyTable/currTable.js
+++ src/currencyTable/currTable.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import CurrencyConverter from "../currencies";
 
 export default class CurrTable extends Component {
+    constructor(props) {
+        super(props);
+        this.cv = new CurrencyConverter();
+        this.state = { currencies: [], sourceCurrency: "UAH", rates: {} };
+    }
+
+    componentDidMount() {
+        this.setState({ currencies: this.cv.currenciesList() });
+        this.cv.currenciesList().forEach((code) => {
+            this.getRate(code);
+        });
+        console.log('rates: ', this.state.rates)
+    }
+
+    getRate = (code) => {
+        this.cv.getLatestRate(this.state.sourceCurrency, code)
+            .then((rate) => {
+                console.log(code, rate);
+                this.setState({ rates: { ...this.state.rates, [code]: rate } });
+            });
+    };
+
     render() {
         return (
             <ul>
-                <li>xxx</li>
+                {this.state.currencies.map((code) => (<li key={code}>{code} ({1/this.state.rates[code]})</li>))}
             </ul>
         );
     }
