433a4d0350bf1479db2fc99695f96088b9d46b29
rostyslavnikitin <rosstindev@gmail.com>
2020-10-29 10:41:45 +0200

    add currencies table component

---
+++ src/currencyTable/currTable.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CurrTable extends Component {
+    render() {
+        return (
+            <ul>
+                <li>xxx</li>
+            </ul>
+        );
+    }
+}
