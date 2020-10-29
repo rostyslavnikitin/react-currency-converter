94e87e80245e82a3fdc32548e03e3561a354072c
rostyslavnikitin <rosstindev@gmail.com>
2020-10-29 10:43:04 +0200

    create routing for existing components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Converter from "./converter/converter";
+import CurrTable from "./currencyTable/currTable";
+import {
+    Switch,
+    Route,
+    withRouter,
+    Redirect,
+    Link
+} from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Converter/>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        const { history } = this.props;
+        return (
+            <div className="App">
+                <header className="x">
+                    <Link to="/converter">Converter</Link>
+                    <Link to="/currencies">Currencies list</Link>
+                </header>
+                <Switch>
+                    <Route history={history} path="/currencies" component={CurrTable} />
+                    <Route history={history} path="/converter" component={Converter} />
+                    <Redirect from='/' to='/converter'/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withRouter(App);
