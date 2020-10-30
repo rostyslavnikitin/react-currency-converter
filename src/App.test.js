6d45dd2c3b96cf271c8ae62b3d04edd43d09c14b
rostyslavnikitin <rosstindev@gmail.com>
2020-10-30 16:22:05 +0200

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render, screen } from '@testing-library/react';
 import App from './App';
-//
+
 // test('renders converter link', () => {
 //   render(<App />);
-//   const linkElement = screen.getByText(/converter/i);
+//   const linkElement = screen.getByText(/Converter/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
