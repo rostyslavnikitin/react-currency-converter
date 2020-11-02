1c1c91581d567d6bf022bbee47b2926b3dcca6ad
rostyslavnikitin <rosstindev@gmail.com>
2020-11-02 14:40:52 +0200

    flag class moved from styles to stylesheet

--- src/flag/flag.js
+++ src/flag/flag.js
@@ -1,13 +1,14 @@
 import React, {Component} from "react";
 import codes from "currency-codes";
+import "./flag.css";
 
 export default class Flag extends Component {
     render() {
         return (
             <div
                 title={`${codes.code(this.props.flag).currency}`}
-                className={`currency-flag currency-flag-${this.props.flag.toLowerCase()}`}
-                style={{ 'marginBottom': '-0.15em' }}> </div>
+                className={`flag currency-flag currency-flag-${this.props.flag.toLowerCase()}`}
+            > </div>
         );
     }
 }
